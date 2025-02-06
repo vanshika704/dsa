afbfe1afb41971762d9a16da0a497fae7fabfc0a
vanshika704 <sharmavanshi704@gmail.com>
2025-02-06 19:57:25 +0530

    ..

--- Arrays/searching.java
+++ Arrays/searching.java
@@ -7,12 +7,24 @@ public class searching {
         int result = search(arr, target);
         System.out.println(result);
     }
-    public static int search(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                return i;
-            }
+    // public static int search(int[] arr, int target) {
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (arr[i] == target) {
+    //             return i;
+    //         }
+    //     }
+    //           return -1;
+    // }
+
+public static int search(int[] arr, int target) {
+    int left = 0;   
+    int right = arr.length - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+         return mid;
         }
-              return -1;
     }
+    return -1;
+}
 }
